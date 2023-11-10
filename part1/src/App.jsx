507c4caaf4201d1ba628cd44ba276598e2e9a24b
rayhanasyraff <rayhanasyraffhjamran@gmail.com>
2023-11-11 00:09:57 +0800

    multiple-components

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -3,19 +3,22 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const Hello = () => {
+  return (
+    <div>
+      <p>Hello world</p>
+    </div>
+  )
+}
+
 const App = () => {
-  const now = new Date()
-  const a = 10
-  const b = 20
-  return React.createElement(
-    'div',
-    null,
-    React.createElement(
-      'p', null, 'Hello world, it is ', now.toString()
-    ),
-    React.createElement(
-      'p', null, a, ' plus ', b, ' is ', a + b
-    )
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello />
+      <Hello />
+      <Hello />
+    </div>
   )
 }
 
