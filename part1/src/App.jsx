d6fc71c0f71eff65e376d31c32402ba0758236d4
rayhanasyraff <rayhanasyraffhjamran@gmail.com>
2023-11-11 13:45:37 +0800

    an-event-handler

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -6,13 +6,17 @@ import './App.css'
 const App = () => {
   const [ counter, setCounter ] = useState(0)
 
+  const increaseByOne = () => setCounter(counter + 1)
+  
+  const setToZero = () => setCounter(0)
+
   return (
     <div>
       <div>{counter}</div>
-      <button onClick={() => setCounter(counter + 1)}>
+      <button onClick={increaseByOne}>
         plus
       </button>
-      <button onClick={() => setCounter(0)}> 
+      <button onClick={setToZero}>
         zero
       </button>
     </div>
